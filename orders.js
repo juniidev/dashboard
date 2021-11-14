6c1955384d4e8436f27c5f648b02126fb682586a
Henry <hms745@gmail.com>
2021-11-13 18:57:55 -0600

    Primer commit

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'DJI Mavic Mini',
+        productNumber: '1095',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Sony a7iii',
+        productNumber: '2190',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'iPhone 11',
+        productNumber: '3236',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Huawei D14',
+        productNumber: '3633',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Keyboard RK61 60%',
+        productNumber: '4387',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Sony 50mm Lenses',
+        productNumber: '7258',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'GoPro Hero 10',
+        productNumber: '6534',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
